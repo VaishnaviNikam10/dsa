de457fc61d9606e015208dcacda3d79e31b5ca24
VaishnaviNikam10 <114681510+VaishnaviNikam10@users.noreply.github.com>
2023-06-08 02:39:30 +0530

    Update dfsog.cpp

--- dfsog.cpp
+++ dfsog.cpp
@@ -1,109 +1,134 @@
 #include<iostream>
-#define max 20
+#define max 10
 #define true 1
 #define false 0
 using namespace std;
-class stack
+class bfsl
 {
-    int v1,v2,m[max][max],v[max];
-    public:
-        int n,e;
-        int st[10];
-        int top;
-        stack();
-        ~stack();
-        void push(int);
-        int pop();
-        void create();
-        void display();
-        void dfs(int v1);
-};
-stack::stack()
-{
-    for(v1=0;v1<max;v1++)
-        v[v1]=false;
-    for(v1=0;v1<max;v1++)
-        for(v2=0;v2<max;v2++)
-            m[v1][v2]=false;
-    for(v1=0;v1<max;v1++)
-        st[v1]=false;
-        top=-1;
-}
-stack::~stack()
-{
-    for(v1=0;v1<max;v1++)
-        v[v1]=false;
-    for(v1=0;v1<max;v1++)
+    typedef struct node1
     {
-        for(v2=0;v2<max;v2++)
-            m[v1][v2]=false;    
-    }
-}
-void stack::push(int item)
+        int vertex;
+        struct node1* next;
+    }node;
+    node *head[max];
+    int queue[max],visited[max],front,rear;
+    public:
+        bfsl();
+        void create();
+        void bfs(int);
+};
+bfsl::bfsl()
 {
-    st[++top]=item;
+    int v1;
+    for(v1=0;v1<max;v1++)
+        visited[v1]=false;
+    front=rear=-1;
+    for(v1=0;v1<max;v1++)
+        head[v1]=NULL;
 }
-int stack::pop()
-{
-    int item;
-    item=st[top];
-    top--;
-    return item;
-}
-void stack::create()
+void bfsl::create()
 {
     int v1,v2;
-    cout<<"\n Enter total no. of nodes"<<endl;
-    cin>>n;
-    cout<<"Enter total no. of edges"<<endl;
-    cin>>e;
-    for(int i=0;i<e;i++)
+    node *news,*first;
+    char ans;
+    do
     {
-        cout<<"enter edge"<<endl;
+        cout<<"\n Enter edges"<<endl;
         cin>>v1;
         cin>>v2;
-        m[v1][v2]=true;
-        m[v2][v1]=true;
-
-    }
-}
-void stack::display()
-{
-    cout<<"\n Adjency matrix is:"<<endl;
-    for(v1=0;v1<n;v1++)
-    {
-        for(v2=0;v2<n;v2++)
+        if(v1>=max || v2>=max)
+            cout<<"Invalid edge"<<endl;
+        else
         {
-            cout<<m[v1][v2]<<" ";
+        news=new node;
+        if(news==NULL)
+            cout<<"\n insufficient memory"<<endl;
+        news->vertex=v2;
+        news->next=NULL;
+        first=head[v1];
+        if(first==NULL)
+            head[v1]=news;
+        else{
+            while(first->next!=NULL)
+                first=first->next;
+            first->next=news;
         }
-        cout<<endl;
-    }
-}
-void stack::dfs(int v1)
-{
-    int v2;
-    push(v1);
-    while(top!=-1)
-    {
-        v1=pop();
-        if(v[v1]==false)
+        
+        news=new node;
+        news->vertex=v1;
+        news->next=NULL;
+        first=head[v2];
+        if(first==NULL)
+            head[v2]=news;
+        else
         {
-            cout<<v1<<" ";
-            v[ v1]=true;
+            while(first->next!=NULL)
+                first=first->next;
+            first->next=news;
         }
-        for(v2=0;v2<n;v2++)
-            if(m[v1][v2]==true && v[v2]==false)
-                push(v2);
+        }
+        cout<<"\n You want to insert more edges(enter y/n): ";
+        cin>>ans;
+        
+    }while(ans=='y');
+}
+void bfsl::bfs(int v1)
+{
+    int i;
+    node *first;
+    queue[++rear]=v1;
+    while(front!=rear)
+    {
+        i=queue[++front];
+        if(visited[i]==false)
+        {
+            cout<<endl<<i<<" ";
+            visited[i]=true;
+        }
+        first=head[i];
+        while(first!=NULL)
+        {
+            if(visited[first->vertex]==false)
+                queue[++rear]=first->vertex;
+                first=first->next;
+        }
     }
 }
 int main()
 {
-    stack s;
-    s.create();
-    s.display();
     int v;
-    cout<<"\nenter starting vertex"<<endl;
+    bfsl bs;
+    bs.create();
+    cout<<"\n Enter starting vertex; "<<endl;
     cin>>v;
-    s.dfs(v);
+    cout<<"\n\nBFS TRAVERSAL :\n";
+    bs.bfs(v);
     return 0;
 }
+
+/*Enter edges
+0 2
+You want to insert more edges(enter y/n): y
+Enter edges
+0 1
+You want to insert more edges(enter y/n): y
+Enter edges
+2 0
+You want to insert more edges(enter y/n): y
+Enter edges
+1 2
+You want to insert more edges(enter y/n): y
+Enter edges
+3 3
+You want to insert more edges(enter y/n): y
+Enter edges
+2 3
+You want to insert more edges(enter y/n): n
+Enter starting vertex; 
+2
+BFS TRAVERSAL :
+
+2 
+0 
+1 
+3 */ 
